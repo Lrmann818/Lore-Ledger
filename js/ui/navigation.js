128f0379dbb81427c1d6e2999360f7c064e1f716
willowsage <lrdunn301@gmail.com>
2026-04-09 17:09:05 -0500

    cleanup: hub now default for first time user

--- js/ui/navigation.js
+++ js/ui/navigation.js
@@ -65,7 +65,7 @@ export function initTopTabsNavigation(deps = {}) {
     tabsRootSelector = ".tabs",
     tabSelector = ".tab[data-tab]",
     pageIdPrefix = "page-",
-    defaultTab = "tracker",
+    defaultTab = "hub",
     updateHash = true,
     canActivateTab = () => true
   } = deps;
